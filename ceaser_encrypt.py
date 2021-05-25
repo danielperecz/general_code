e32563d3a603fb62399d49cf7f5ed36aa708e28e
Daniel <42898223+danielperecz@users.noreply.github.com>
2021-05-26 00:05:27 +0100

    Create ceaser_encrypt.py

---
+++ ceaser_encrypt.py
@@ -0,0 +1,25 @@
+sin = input()
+
+n = 13
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+lower_upper_set = set(lower_case + lower_case.upper())
+
+
+def encrypt_char(s):
+    decrypted_i = lower_case.index(s)
+    encrypted_i = (decrypted_i + n) - 26
+    return lower_case[encrypted_i]
+
+
+def encrypt_message():
+    d = {k: encrypt_char(k) for k in lower_case}
+    out = ""
+    for c in sin:
+        if c in lower_upper_set:
+            out += d[c] if c.islower() else d[c.lower()].upper()
+        else:
+            out += c
+    return out
+
+
+print(encrypt_message())
